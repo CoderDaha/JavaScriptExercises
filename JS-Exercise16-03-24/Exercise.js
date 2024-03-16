c8496adc24c5bdb2c981361423d6a5f29cdf9710
CoderDaha <DahaYusuf@outlook.com>
2024-03-16 15:03:07 +0000

    First JS Introduction

---
+++ JS-Exercise16-03-24/Exercise.js
@@ -0,0 +1,29 @@
+// ------ Exercise 1---------------
+let myNumber = 20;
+
+console.log(myNumber);
+
+let octopusLimbs = 8;
+console.log(octopusLimbs)
+
+// ------ Exercise 2-------------
+
+const boilingPointC = 100;
+console.log(boilingPointC);
+
+const boilingPointF = 212;
+console.log(boilingPointF);
+
+// -------- Exercise 3-------------
+let firstNumber = 20;
+
+let secondNumber = 80;
+
+let thirdNumber = firstNumber + secondNumber
+console.log(thirdNumber);
+
+let forthNumber = thirdNumber * 100
+console.log(forthNumber)
+
+let remainder = thirdNumber % forthNumber
+console.log(remainder);
